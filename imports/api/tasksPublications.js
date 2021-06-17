1b2d8c228bdc31b2f11a470d7e7588cf13c95288
isanurishav <85946359+isanurishav@users.noreply.github.com>
2021-06-17 13:23:16 +0530

    Update tasksPublications.js

--- imports/api/tasksPublications.js
+++ imports/api/tasksPublications.js
@@ -2,5 +2,5 @@ import { Meteor } from 'meteor/meteor';
 import { TasksCollection } from '../db/TasksCollection';
 
 Meteor.publish('tasks', function publishTasks() {
-  return TasksCollection.find({ userId: this.userId });
+  return TasksCollection.find({});
 });
