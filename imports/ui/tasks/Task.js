72cdd2a1afbc5261923ca12b93dd10c63346c255
isanurishav <85946359+isanurishav@users.noreply.github.com>
2021-06-17 13:25:32 +0530

    Update Task.js

--- imports/ui/tasks/Task.js
+++ imports/ui/tasks/Task.js
@@ -3,24 +3,15 @@ import { Box, Button, HStack, Stack, Checkbox } from '@chakra-ui/react';
 
 export const Task = ({ task, onMarkAsDone, onDelete }) => (
   <HStack mt={4}>
-    <Box w="80%">
-      <Checkbox
-        colorScheme="green"
-        isChecked={task.done}
-        onChange={() => onMarkAsDone(task)}
-      >
+    <Box w="70%">
+      
         {task.description}
-      </Checkbox>
+      
     </Box>
-    <Stack w="20%" justify="flex-end" direction="row">
-      <Button
-        colorScheme="red"
-        variant="outline"
-        size="xs"
-        onClick={() => onDelete(task)}
-      >
-        Remove
-      </Button>
-    </Stack>
+    <Box w="30%">
+    
+        by-{task.userId}
+    </Box>
+ 
   </HStack>
 );
